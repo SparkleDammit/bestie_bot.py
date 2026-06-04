cfc745c933bdeb688036651a7b364b6227b32fef
SparkleDammit <162503136+SparkleDammit@users.noreply.github.com>
2026-06-04 19:48:34 +0100

    Update main.py one tree line

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ async def resetcount_error(interaction: discord.Interaction, error: app_commands
 @client.event
 async def on_ready():
     guild = discord.Object(id=1487446782219911241)
-    await tree.clear_commands(guild=guild)
+    tree.clear_commands(guild=guild)
     synced = await tree.sync(guild=guild)
     print(f"Logged in as {client.user}")
     print(f"Synced {len(synced)} command(s) to guild {guild.id}:")
